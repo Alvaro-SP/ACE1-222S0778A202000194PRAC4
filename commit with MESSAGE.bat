15907da898331dbff4337d168e1a8adf93f1f324
Alvaro-SP <socop2412@gmail.com>
2022-09-17 21:50:49 -0600

    commit

--- commit with MESSAGE.bat
+++ commit with MESSAGE.bat
@@ -1,13 +1,13 @@
 
 @echo off
 echo %~dp0
-git remote add all https://github.com/Alvaro-SP/-ACE1-222S0778A202000194PRAC4.git
+git remote add all https://github.com/Alvaro-SP/ACE1-222S0778A202000194PRAC4.git
 
 SET /P NOMBRE=WRITE COMMIT:
 git add .
 git commit -m "%NOMBRE%"
 git push
-git push https://Alvaro-SP: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@github.com/Alvaro-SP/-ACE1-222S0778A202000194PRAC4.git
+git push https://Alvaro-SP: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@github.com/Alvaro-SP/ACE1-222S0778A202000194PRAC4.git
 pause
 
 
