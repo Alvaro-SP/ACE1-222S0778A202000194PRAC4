c16e54a14ffa70a7bc885e441a8bc3df7f6e5dff
Alvaro-SP <socop2412@gmail.com>
2022-09-17 21:49:36 -0600

    starting

---
+++ COMMIT.bat
@@ -0,0 +1,12 @@
+
+@echo off
+echo %~dp0
+@REM git remote add all https://github.com/Alvaro-SP/PROYECTO-COMPI2.git
+
+git add .
+git commit -m "commit"
+git push
+git push https://Alvaro-SP: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@github.com/Alvaro-SP/-ACE1-222S0778A202000194PRAC4.git
+@REM pause
+
+
