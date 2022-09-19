c06935b522aca0deeb3bbaccae3dca85ef4c6b88
Alvaro-SP <socop2412@gmail.com>
2022-09-19 12:55:22 -0600

    commit

--- COPYFILE.bat
+++ COPYFILE.bat
@@ -1,9 +1,9 @@
 
 chcp 65001
 @REM C:\Users\alvaro\OneDrive - Facultad de Ingeniería de la Universidad de San Carlos de Guatemala\6 S E X T O   S E M E S T R E\arquiiiiiiii  1\LAB ARQUI\PRACTICAS\4\ACE1-222S0778A202000194PRAC4\P4.asm
-copy "C:\Users\alvaro\OneDrive - Facultad de Ingeniería de la Universidad de San Carlos de Guatemala\6 S E X T O   S E M E S T R E\arquiiiiiiii  1\LAB ARQUI\PRACTICAS\3\ACE1-222S0778A202000194PRAC4\P4.asm" "C:\MASM\MASM611\BIN\P4.asm"
-copy "C:\Users\alvaro\OneDrive - Facultad de Ingeniería de la Universidad de San Carlos de Guatemala\6 S E X T O   S E M E S T R E\arquiiiiiiii  1\LAB ARQUI\PRACTICAS\3\ACE1-222S0778A202000194PRAC4\MACROS.asm" "C:\MASM\MASM611\BIN\MACROS.asm"
-copy "C:\Users\alvaro\OneDrive - Facultad de Ingeniería de la Universidad de San Carlos de Guatemala\6 S E X T O   S E M E S T R E\arquiiiiiiii  1\LAB ARQUI\PRACTICAS\3\ACE1-222S0778A202000194PRAC4\ARCHIVOS.asm" "C:\MASM\MASM611\BIN\ARCHIVOS.asm"
+copy "C:\Users\alvaro\OneDrive - Facultad de Ingeniería de la Universidad de San Carlos de Guatemala\6 S E X T O   S E M E S T R E\arquiiiiiiii  1\LAB ARQUI\PRACTICAS\4\ACE1-222S0778A202000194PRAC4\P4.asm" "C:\MASM\MASM611\BIN\P4.asm"
+copy "C:\Users\alvaro\OneDrive - Facultad de Ingeniería de la Universidad de San Carlos de Guatemala\6 S E X T O   S E M E S T R E\arquiiiiiiii  1\LAB ARQUI\PRACTICAS\4\ACE1-222S0778A202000194PRAC4\MACROS.asm" "C:\MASM\MASM611\BIN\MACROS.asm"
+copy "C:\Users\alvaro\OneDrive - Facultad de Ingeniería de la Universidad de San Carlos de Guatemala\6 S E X T O   S E M E S T R E\arquiiiiiiii  1\LAB ARQUI\PRACTICAS\4\ACE1-222S0778A202000194PRAC4\ARCHIVOS.asm" "C:\MASM\MASM611\BIN\ARCHIVOS.asm"
 DEL /F /A "C:\MASM\MASM611\BIN\P4.exe"
 cd "C:\Program Files (x86)\DOSBox-0.74-3\"
 DOSBox.exe
